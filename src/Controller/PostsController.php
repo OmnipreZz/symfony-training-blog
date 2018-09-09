d60ce628f02e981eef09705762d2b3f38b93c939
Geoffrey <geoffrey.frioli@gmail.com>
2018-09-09 22:49:09 +0200

    CRUD post ok

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Post;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class PostsController extends AbstractController
 {
     /**
      * @Route("/posts", name="posts")
      */
-    public function index()
+    public function index(PostRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Post::class);
-        $posts = $repo->findAll();
+        // $repo = $this->getDoctrine()->getRepository(Post::class);
+        $posts = $repo->findBy(array(), array('CreatedAt' => 'desc'));
         return $this->render('posts/index.html.twig', [
             'posts' => $posts
         ]);
@@ -31,14 +35,61 @@ class PostsController extends AbstractController
     }
 
     /**
-     * @Route("/post/{id}", name="posts_show")
+     * @Route("/post/new", name="post_create")
+     * @Route("/post/{id}/edit", name="post_edit")
      */
-    public function show($id)
+    public function form(Post $post = null, Request $request)
     {
-        $repo = $this->getDoctrine()->getRepository(Post::class);
+        if(!$post) {
+            $post = new Post();
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$post->getId()) {
+                $post->setCreatedAt(new \DateTime());
+            }
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('post_show', ['id' => $post->getId()]);
+        }
+
+        return $this->render('posts/create.html.twig', [
+            'formPost' => $form->createView(),
+            'editMode' => $post->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post_show")
+     */
+    public function show(PostRepository $repo, $id)
+    {
         $post = $repo->find($id);
         return $this->render('posts/show.html.twig', [
             'post' => $post
         ]);
     }
+
+    /**
+     * @Route("/posts/delete/{id}", name="post_delete")
+     */
+    public function delete(Post $post, Request $request )
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirectToRoute('posts');
+    }
+
+
+
 }
